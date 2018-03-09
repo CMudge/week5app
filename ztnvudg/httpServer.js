17e38df3c297c5efad8477c33c7e670244457087
CMudge <clairemudge34@gmail.com>
2018-03-09 08:35:11 -0500

    Tested query that retrieves data from PostGIS in GeoJSON format

--- ztnvudg/httpServer.js
+++ ztnvudg/httpServer.js
@@ -1,11 +1,8 @@
 
-
-
-
 // express is the server that forms part of the nodejs program
 var express = require('express');
 var path = require("path");
-var fs  = require("fs")
+var fs  = require("fs") // This library is needed
 var app = express();
 
 
@@ -19,17 +16,31 @@ for (var i = 0; i < configarray.length; i++) {
 	var split = configarray[i].split(':');
 	config[split[0].trim()] = split[1].trim();
 }
+
+
 var pg = require('pg');
 var pool = new pg.Pool(config);
 
 
-app.get('/postgistest', function (req,res) {
-pool.connect(function(err,client,done) {
-	if(err){
-		console.log("not able to get connection "+ err);
-		res.status(400).send(err);
-	}
-	client.query('SELECT name FROM united_kingdom_counties',function(err,result) {
+app.get('/getPOI', function (req,res) {
+	pool.connect(function(err,client,done) {
+		if(err){
+			console.log("not able to get connection "+ err);
+			res.status(400).send(err);
+		}
+		
+		/* use the inbuilt geoJSON functionality and create the required geoJSON format using a query adapted from here:
+		http://www.postgresonline.com/journal/archives/267-Creating-GeoJSON-FeatureCollections-with-JSON-and-PostGIS-functions.html, accessed 4th January 2018 */
+		
+		// note that query needs to be a single string with no line breaks so build it up bit by bit
+	 
+		var querystring = " SELECT 'FeatureCollection' As type, array_to_json(array_agg(f)) As features FROM ";
+		querystring = querystring + "(SELECT 'Feature' As type , ST_AsGeoJSON(lg.geom)::json As geometry, ";
+		querystring = querystring + "row_to_json((SELECT l FROM (SELECT id, name, category) As l )) As properties";
+		querystring = querystring + " FROM united_kingdom_poi As lg limit 100 ) As f ";
+		console.log(querystring);
+		client.query(querystring,function(err,result){
+		//call `done()` to release the client back to the pool
 		done();
 		if(err){
 			console.log(err);
@@ -39,6 +50,12 @@ pool.connect(function(err,client,done) {
 		});
 	});
 });
+
+
+
+
+
+
 
 	// adding functionality to allow cross-domain queries when PhoneGap is running a server
 	app.use(function(req, res, next) {
